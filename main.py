8d88ffbf505230feda0f38b47abd8ee774153657
Willian Ribeiro <willianribeirocorrea@gmail.com>
2020-12-07 02:44:15 -0300

    First commit

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def convolution(parameter):
     # Use a breakpoint in the code line below to debug your script.
     print("Do convolution with parameter: ", parameter)
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     # Audio input
     phrase = wave.open("Adria nao faz licao.wav", "rb")
-    word = wave.open("Adria Recorte.wav", "rb") # este eh um recorte do primeiro audio, contendo apenas 1 palavra
-    # word = wave.open("Adria.wav", "rb") # Este eh outro audio com a palavra repetida
+    word = wave.open("Adria Recorte.wav", "rb")  # este eh um recorte do primeiro audio, contendo apenas 1 palavra
+    # word = wave.open("Adria.wav", "rb")  # Este eh outro audio com a palavra repetida
 
     # Convert to bytes
     phrase_soundwave = phrase.readframes(-1)
@@ -26,4 +27,3 @@ if __name__ == '__main__':
     np.convolve(phrase_signal, word_signal)
 
     print("Done")
-
